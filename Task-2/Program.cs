ed2f98631fbae37bf2606aba21fbbb8ca11af5b5
vlad <zubrov@list.ru>
2020-12-17 00:38:36 +0300

    4

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -8,8 +8,19 @@ namespace Task_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            Console.WriteLine("Введите минимальную температуру");
+            string TempMin = Console.ReadLine();
+            Console.WriteLine("Введите максимальную температуру");
+            string TempMax = Console.ReadLine();
+            double TempMid = (Convert.ToDouble(TempMin) + Convert.ToDouble(TempMax)) / 2;
+            Console.WriteLine($"Средняя температурура {Convert.ToString(TempMid)}");
+            Console.ReadLine();
+
+
+
         }
     }
 }
