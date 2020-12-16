6acfaf7393da55f4ef241f5b4984bfde4c4da1ab
vlad <zubrov@list.ru>
2020-12-17 01:23:05 +0300

    1

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -8,8 +8,43 @@ namespace Task_2
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static void Main()
         {
+            Console.WriteLine("Введите месяц");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите температуру");
+            string b = Console.ReadLine();
+            int Month = Convert.ToInt32(a);
+            switch (Month)
+            {
+                case 11:
+                case 0:
+                case 1:
+                    Console.WriteLine(Convert.ToBoolean(Convert.ToInt32(b)>0)? "Дождливая зима" : "Зима");
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("Весна");
+                    break;
+                case 5:
+                case 6:
+                case 7:
+                    Console.WriteLine("Лето");
+                    break;
+                case 8:
+                case 9:
+                case 10:
+                    Console.WriteLine("Осень");
+                    break;
+                default:
+                    Console.WriteLine("Укажите значение от 0 до 11");
+                    break;
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
